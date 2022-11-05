2e84613632cb8e762b5c9ec3d1edf935b36c2905
Shooter-06 <john190curry@example.com>
2022-11-05 14:33:13 -0700

    reviews.php

---
+++ app/controllers/review.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\controlllers;
+
+#[\app\filters\Login]
+#[\app\filters\Profile]
+class review extends app\core\Controller{
+	public function index(){
+				
+	}
+}
---
+++ app/models/Review.php
@@ -0,0 +1,54 @@
+<?php
+namespace app\models;
+
+
+class Review extends app\core\Model{
+	public function get($review_id){
+		$SQL = "SELECT * FROM review WHERE review_id=:review_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['review_id'=>$review_id]);
+		$STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Review');
+		return $STMT->fetch();
+	}
+
+	public function insert(){
+		$SQL = "INSERT INTO review(product_id, profile_id, review, date) VALUES (:product_id,:profile_id, :review,:date)";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['product_id'=>$this->product_id,
+						'profile_id'=>$this->profile_id,
+						'review'=>$this->review,
+						'date'=>$this->date]);
+		return self::$_connection->lastInsertId();
+	}
+
+	public function update(){
+		$SQL = "UPDATE review SET product_id=:product_id, profile_id=:profile_id, review=:review, date=:date WHERE review_id=:review_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['product_id'=>$this->product_id,
+						'profile_id'=>$this->profile_id,
+						'review'=>$this->review,
+						'date'=>$this->date]);
+	}
+
+	public function delete(){
+		$SQL = "DELETE FROM review WHERE review_id=:review_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['review_id'=>$this->review_id]);
+	}
+
+	public function getProfile(){
+		$SQL = "SELECT * FROM profile WHERE profile_id=:profile_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['profile_id'=>$this->profile_id]);
+		$STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Profile');
+		return $STMT->fetch();
+	}
+
+	public function getProducts(){
+		$SQL = "SELECT * FROM product WHERE product_id=:product_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['product_id'=>$this->product_id]);
+		$STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Profile');
+		return $STMT->fetch();
+	}
+}
