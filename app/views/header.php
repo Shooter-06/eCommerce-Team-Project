4501e25d19f35f7c6dd56025c16d304582ed8458
BakaDobe <anthoniuspowellus@gmail.com>
2022-11-20 11:40:50 -0500

    logo/shopping cart and search

--- app/views/header.php
+++ app/views/header.php
@@ -12,7 +12,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="C:\xampp\htdocs\app\css\header.css">
 	<link rel="stylesheet" href="app\CSS\site.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
+	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous"> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 
@@ -47,30 +48,34 @@
 	</style>
  
 	<!-- Navigation -->
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/Main/index">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+	  <a href="/Main/index"><img src="/Images/workshop.png" alt="" style="max-width: 100px;" style="max-height: 100px;"></a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/User/index">LOG IN <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/Product/create">POST PRODUCT</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/Profile/index">MY PROFILE</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item">
+	        <a class="nav-link" href="/User/index">LOG IN</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="/Product/create">POST PRODUCT</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="/Profile/index">MY PROFILE</a>
+	      </li>
+	      <li class="nav-item">
+	        <i class="fa-solid fa-magnifying-glass"></i>
+	        <i class="fa-sharp fa-solid fa-cart-shopping"></i>
+	      </li>
+	    </ul>
+	    <!-- <form class="form-inline my-2 my-lg-0">
+	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+	    </form> -->
+	  </div>
+	</nav>
 
 	<!-- <div class='container'>
 		<div class='logo'>
