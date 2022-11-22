d57d98392f92eecde3c5174864740afc1405de5e
BakaDobe <anthoniuspowellus@gmail.com>
2022-11-22 14:01:40 -0500

    updated html

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -1,12 +1,12 @@
 <body>
 	<style>
 		.info{
-		    display: flex;
 		    width: 100%;
 		    background-color: rgb(122, 122, 212);
 		    color: white;
-		    font-size: 18px;
 		    justify-content: center;
+		    margin-bottom: 1.5rem;
+
 		}
 
 		#home{
@@ -34,9 +34,53 @@
 			transition: 0.3s all;
 		}
 
+		.shop_contents{
+			display: grid;
+			grid-template-columns: repeat(auto-fit, minmax(220px,auto));
+			gap: 1.5rem;
+		}
 
+		.product_box{
+			position: relative;
+		}
+
+		.product_box:hover{
+			opacity: 0.7;
+			padding: 10px;
+			border: 1px solid var(--text-color);
+			transition: 0.4s;
+		}
+
+		.img-fluid{
+			width: 100%;
+			height: auto;
+			margin-bottom: 0.5rem;
+		}
+
+		.product_title{
+			font-size: 1.1rem;
+			font-weight: 600;
+			text-transform: uppercase;
+			margin-bottom: 0.5rem;
+		}
+
+		h5{
+			font-weight: 600;
+			text-align: center;
+			justify-content: center;
+		}
+
+		i{
+			cursor: pointer;
+		}
 
 	</style>
+
+
+
+
+
+
 	<div class="info">
 		<h5>Our stock Inventory</h5>
 	</div>
@@ -57,10 +101,79 @@
 	?>	 -->
 
 
-	<!-- <section id="home">
-		<div class=""></div>
-	</section> -->
-	<section id="featured">
+	<section class="shop_container">
+		<div class="shop_contents mx-auto container-fluid">
+			<div class="product_box text-center">
+				<div class="card">
+					<img class="img-fluid" src="/Images/yellownike.jpg">
+					<div class="card-contents">
+						<h2 class="product_title">Kyrie 6 Low</h2>
+						<span class="price">$164.99</span><br>
+						<button class="btnpurchase">Buy Now</button>
+						<i class="fa-sharp fa-solid fa-cart-shopping"></i>
+					</div>	
+				</div>
+				
+			</div>
+			<div class="product_box text-center">
+				<div class="card">
+					<img class="img-fluid" src="/Images/airmax.jpg">
+					<div class="card-contents">
+						<h2 class="product_title">Kyrie 6 Low</h2>
+						<span class="price">$164.99</span><br>
+						<button class="btnpurchase">Buy Now</button>
+						<i class="fa-sharp fa-solid fa-cart-shopping"></i>
+					</div>	
+				</div>
+				
+			</div> <div class="product_box text-center"> <div class="card">
+			<img class="img-fluid" src="/Images/bluewhitenike.jpg"> <div
+			class="card-contents"> <h2 class="product_title">Kyrie 6
+			Low</h2> <span class="price">$164.99</span><br> <button
+			class="btnpurchase">Buy Now</button> <i class="fa-sharp
+			fa-solid fa-cart-shopping"></i> </div>	 </div>
+				
+			</div>
+			<div class="product_box text-center">
+				<div class="card">
+					<img class="img-fluid" src="/Images/redwhite.jpg">
+					<div class="card-contents">
+						<h2 class="product_title">Kyrie 6 Low</h2>
+						<span class="price">$164.99</span><br>
+						<button class="btnpurchase">Buy Now</button>
+						<i class="fa-sharp fa-solid fa-cart-shopping"></i>
+					</div>	
+				</div>
+				
+			</div>
+			<div class="product_box text-center">
+				<div class="card">
+					<img class="img-fluid" src="/Images/greynike.jpg">
+					<div class="card-contents">
+						<h2 class="product_title">Kyrie 6 Low</h2>
+						<span class="price">$164.99</span><br>
+						<button class="btnpurchase">Buy Now</button>
+						<i class="fa-sharp fa-solid fa-cart-shopping"></i>
+					</div>	
+				</div>
+				
+			</div>
+			<div class="product_box text-center">
+				<div class="card">
+					<img class="img-fluid" src="/Images/yellownike.jpg">
+					<div class="card-contents">
+						<h2 class="product_title">Kyrie 6 Low</h2>
+						<span class="price">$164.99</span><br>
+						<button class="btnpurchase">Buy Now</button>
+						<i class="fa-sharp fa-solid fa-cart-shopping"></i>
+					</div>	
+				</div>
+				
+			</div>
+		</div>
+	</section>
+
+	<!-- <section id="featured">
 		<div class="row mx-auto container-fluid">
 			<div class="product text-center col-lg-3 col-md-4 col-12">
 				<img class="img-fluid mb-3" src="/Images/kyrie.jpg">
@@ -77,28 +190,19 @@
 				<button class="btnpurchase">Buy Now</button>
 			</div>
 		</div>
-	</section>
-	<!-- <div class="items">
-		<div class="item-Listed">
-			<img src="/Images/kyrie.jpg" alt="img" id="card-img">
-			<p id="item-name">Kyrie 6 Low</p>
-		</div>
-		<div>
-			<i class="fa fa-shopping-cart" id= add-to-cart></i>
-			<h id="item-price">Price: $165</h>
-		</div>
-			
-		</div> -->
+	</section> -->
+
+
 	
 	<p>Say something about the products</p>
 	<form action='/Review/add/<?=$data->review_id?>' method="post">
-		<div class="review">
-			<textarea id='form-control' name="review"  rows="2" cols="25" placeholder="Say something"></textarea>
-			<button type="submit" name='action' class='btn btn-primary'><i class='bi-send'></i></button>
-		</div>
-	</form>
+	<div class="review">
+		<input class='form-control' type="text" name="review" placeholder="Say something" />
+	<button type="submit" name='action' class='btn btn-primary'><i class='bi-send'></i></button>
+	</div></form>
 
 	</div>
+
 
 	<a href="/Product/create" class= "backBtn">Add a new Product</a>
 	<br>
@@ -110,4 +214,11 @@
 
 </body>
 
+<footer>
+	<p>this project was developed by:</p>
+	<?=$this->view('footer');?>
+</footer>
+<!-- <footer>
+	<?php $this->view('footer'); ?>
+</footer> -->
 </html>
